c832224a0b8cbf8b893b5d4982d367abe806a97a
mchoeti <mchoeti@gmail.com>
2021-06-14 12:58:59 +0200

    everyone here and ShortCut APP

---
+++ React/Project/App.js
@@ -0,0 +1,17 @@
+// rcc
+// rce
+// rcep
+// check things in Cheat Sheet.
+
+// rcc
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return ( 
+            <div>
+
+            </div>
+        )
+    }
+}
