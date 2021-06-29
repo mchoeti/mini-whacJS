cd3970bc519adcba3ab8dc52aca5a747aa882303
Mchoeti <lastcrusade@gmx.at>
2021-06-29 15:23:18 +0200

    holiday check

---
+++ CodeMe/helloWorldBackup.js
@@ -0,0 +1,13 @@
+console.log("Hello world i am back");
+
+function status(motivation) {
+    if (motivation === 0) {
+        console.log("Welcome to holiday");
+    } else {
+        console.log(" Back to work !!!");
+    }
+    return motivation;
+}
+
+// Oje es geht wieder zruck in di HOCKN
+console.log(status(1));
