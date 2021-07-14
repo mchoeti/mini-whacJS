8568138bfa549af684234e90a043c9648281dd73
Mchoeti <lastcrusade@gmx.at>
2021-07-14 18:13:35 +0200

    map and Filter

--- CodeMe/OOP/mapOnProto.js
+++ CodeMe/OOP/mapOnProto.js
@@ -4,6 +4,9 @@ var s = [23, 65, 98, 5];
 Array.prototype.myMap = function (callback) {
     var newArray = [];
     // Only change code below this line
+    for (let index = 0; index < this.length; index++) {
+        newArray.push(callback(this[index]));
+    }
 
     // Only change code above this line
     return newArray;
@@ -12,3 +15,5 @@ Array.prototype.myMap = function (callback) {
 var new_s = s.myMap(function (item) {
     return item * 2;
 });
+
+console.log(new_s);
