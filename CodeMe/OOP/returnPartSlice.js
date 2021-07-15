2c204ba552b8b4deb6733e9e19c77119c71d123c
Mchoeti <lastcrusade@gmx.at>
2021-07-15 15:23:08 +0200

    slice

---
+++ CodeMe/OOP/returnPartSlice.js
@@ -0,0 +1,20 @@
+//uses 2 paramaters ( begin and End)
+//slice ( begin, end)
+
+// RETURN nicht vergessen :-)
+function sliceArray(anim, beginSlice, endSlice) {
+    // Only change code below this line
+    let newArr = [...inputAnim];
+    newArr = anim.slice(beginSlice, endSlice);
+    return newArr;
+    // Only change code above this line
+}
+var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+
+console.log(sliceArray(inputAnim, 1, 3));
+
+
+// simple sample with    
+// var arr = ["Cat", "Dog", "Tiger", "Zebra"];
+// var newArray = arr.slice(1, 3);
+// console.log(newArray);
---
+++ CodeMe/OOP/test.js
@@ -0,0 +1,10 @@
+function sliceArray(anim, beginSlice, endSlice) {
+    // Only change code below this line
+    let newArr = [...inputAnim];
+    newArr = anim.slice(beginSlice, endSlice);
+    return newArr;
+    // Only change code above this line
+}
+var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+
+console.log(sliceArray(inputAnim, 1, 3));
