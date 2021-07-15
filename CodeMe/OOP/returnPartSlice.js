183d11e6568f58f0a2672fa6b57d788be0b9ba0f
Mchoeti <lastcrusade@gmx.at>
2021-07-15 15:24:47 +0200

    added simple solution

--- CodeMe/OOP/returnPartSlice.js
+++ CodeMe/OOP/returnPartSlice.js
@@ -7,6 +7,9 @@ function sliceArray(anim, beginSlice, endSlice) {
     let newArr = [...inputAnim];
     newArr = anim.slice(beginSlice, endSlice);
     return newArr;
+    // simpler
+    //  return anim.slice(beginSlice, endSlice);
+
     // Only change code above this line
 }
 var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
