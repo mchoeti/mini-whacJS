56f15f57ed5d3dc603eb388ee23139faeb9bc39a
Mchoeti <lastcrusade@gmx.at>
2021-08-03 14:34:43 +0200

    formatted things

--- CodeMe/OOP/nodejsPost.js
+++ CodeMe/OOP/nodejsPost.js
@@ -4,22 +4,22 @@ var data = new FormData();
 data.append('body', '{"18280554_15": "Protokoll"}');
 
 var config = {
-  method: 'put',
-  url: 'https://ecm.graz.at/OTCS/cs.exe/api/v1/nodes/neuVarHier/categories/18280554',
-  headers: { 
-    'otcsticket': 'sampleToken', 
-    ...data.getHeaders()
-  },
-  data : data
+    method: 'put',
+    url: 'https://ecm.graz.at/OTCS/cs.exe/api/v1/nodes/neuVarHier/categories/18280554',
+    headers: {
+        'otcsticket': 'sampleToken',
+        ...data.getHeaders()
+    },
+    data: data
 };
 
 axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
 
-const meineVariable = [ 1,2,3,4,56678,9990,];
+const meineVariable = [1, 2, 3, 4, 56678, 9990, ];
 console.log(meineVariable);
