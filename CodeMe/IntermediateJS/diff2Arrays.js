a522a4165419a2a2f954837addf24b1242a1ecae
Mchoeti <lastcrusade@gmx.at>
2021-08-10 17:09:19 +0200

    added example

--- CodeMe/IntermediateJS/diff2Arrays.js
+++ CodeMe/IntermediateJS/diff2Arrays.js
@@ -1,10 +1,11 @@
 /*
 Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. 
 In other words, return the symmetric difference of the two arrays.
+
+[1, 2, 3, 5], [1, 2, 3, 4, 5] should return [4].
 */
 
 // Starting point
-
 function diffArray(arr1, arr2) {
     var newArr = [];
     console.log("I will be ready ...");
