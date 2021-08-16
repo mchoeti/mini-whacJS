a39353847fe9ec159c6ff088e6267d6fad785570
Mchoeti <lastcrusade@gmx.at>
2021-08-16 16:27:22 +0200

    seek and destroy

--- CodeMe/IntermediateJS/seekDestroy.js
+++ CodeMe/IntermediateJS/seekDestroy.js
@@ -6,11 +6,33 @@ Note: You have to use the arguments object.
 
 */
 
-
-function destroyer(arr) {
+// Ich nutze mal den SPread Operator um die Values die ich übergeben möchte zu bekommen
+function destroyer(arr, ...valuesToRemove) {
     console.log("Wtf should i do?");
-    return arr;
+    const newArray = [...arr];
+    // dann setzen wir filter ein und darin eben die InCLUDE Function.
+    return newArray.filter(element => !valuesToRemove.includes(element));
+
 }
 
 console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
 // result should be 1 
+
+
+// #################################################################
+//  Ein bissal was eingebaut mit Verzögerung
+// #################################################################
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+console.log("Hello");
+sleep(2000)
+    .then(() => {
+        console.log("World!");
+    })
+    .then(() => {
+        sleep(2000)
+            .then(() => {
+                console.log("Goodbye!");
+            });
+    });
