f49aac7f40d1191d36c98d90322bab9111df138f
Mchoeti <lastcrusade@gmx.at>
2021-09-08 09:43:15 +0200

    flat and Set

---
+++ CodeMe/IntermediateJS/flatAndSet.js
@@ -0,0 +1,10 @@
+function uniteUnique(...arrays) {
+    //make an array out of the given arrays and flatten it (using the spread operator)
+    const flatArray = [].concat(...arrays);
+    console.log("Alle Werte", flatArray);
+    // create a Set which clears any duplicates since it's a regular set and not a multiset
+    return [...new Set(flatArray)];
+}
+
+console.log(
+    uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
