2a3ca1659ea4acbfe9d607ab6a65517cd35c460d
Mchoeti <lastcrusade@gmx.at>
2021-09-21 15:48:06 +0200

    a little bid concat

---
+++ CodeMe/BasicAlgo/concat.js
@@ -0,0 +1,10 @@
+const name = "Christian";
+const age = 33;
+
+// geil man nehme die backtik neben dem scharfen ß ` 
+console.log(`Hello it is ${name}`);
+// weil das klappt nicht mit "" oder mit '' 
+console.log("Hi i am ${name}");
+
+// Und Zusammen
+console.log(`Hello my ${name} is and i am ${age} old `);
