b00975e4de065ee0bb93632466f356e54f000a92
Mchoeti <lastcrusade@gmx.at>
2021-10-01 09:34:53 +0200

    a little bid intervall

--- CodeMe/BasicAlgo/setInterval.js
+++ CodeMe/BasicAlgo/setInterval.js
@@ -1,8 +1,18 @@
-'use strict';
+/*jslint node: true */
+
+//const myArray = ["Servas"];
+const myArray = [];
 
 function alertMe() {
-    console.log(" HIHIHIHIHHIHIHIHIH");
+    //let total = myArray.push(" Zusammen");
+    let total = myArray.push("");
+    console.log(total);
+    console.log(myArray);
 }
 
+const timerID = setInterval(alertMe, 2000);
 
-const timerID = setInterval(alertMe, 5000);
+
+
+const myValues = [1, 2, 3, 4, 56, 7, 88, 999];
+console.log(myValues);
