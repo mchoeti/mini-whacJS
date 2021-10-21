4c034a7995e391cd0344af8b8f638294bf174e2b
Mchoeti <lastcrusade@gmx.at>
2021-10-21 17:01:10 +0200

    dot vs brackets

--- CodeMe/Basics/dotVSBrackets.js
+++ CodeMe/Basics/dotVSBrackets.js
@@ -1,5 +1,6 @@
+// J.S. 43
 const christian = {
-    name: "christian",
+    name: "Christian",
     age: 28,
     job: "Coder",
     friends: ["Bud", "Terence"]
@@ -18,3 +19,29 @@ console.log(christian.interresteIn);
 // Update HTML 
 const htmlAnswer = christian[interresteIn];
 document.querySelector(".answer").innerText = htmlAnswer;
+
+// Console Error Meldung
+if (christian[interresteIn]) {
+    console.log(christian[interresteIn]);
+} else {
+    console.log("Falsche Angabe");
+    alert("Falsche Angabe, bitte age, name , job oder friends wählen");
+}
+
+// Adding new properties and values to the object Christian
+christian.location = "Coimbar";
+christian['twitter'] = "@mchoeti";
+console.log(christian);
+
+// Challenge here: 
+// Christian has value friends and his best friend is Bud.
+const firstPart = "Christian hat: " + christian.friends.length;
+//console.log(firstPart);
+const secondPart = "Freunde und sein bester Freund nennt sich: " + christian.friends[0];
+console.log(firstPart, secondPart);
+
+// oder noch cooler mit neuem JS6
+const firstES6 = `${christian.name}` + " hat " + `${christian.friends.length}` + " Freunde";
+const secondES6 = " Sein bester nennt sich " + `${christian.friends[1]}`;
+console.log("Neues JS --------nun Terence---------");
+console.log(firstES6, secondES6);
