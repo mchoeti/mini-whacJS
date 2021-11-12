4b2a970a4802de206b541023631a7c8f7954dee5
Mchoeti <lastcrusade@gmx.at>
2021-11-12 16:23:25 +0100

    a little bid

--- CodeMe/Basics/GuessNumber/script.js
+++ CodeMe/Basics/GuessNumber/script.js
@@ -6,4 +6,11 @@
 // Damit haben wir die Klasse Btn zum stylen und die andere eben für JS und CO
 document.querySelector(".check").addEventListener("click", function () {
     console.log(document.querySelector(".guess").value);
+    const guess = document.querySelector(".guess").value;
+    console.log("This is our gues", guess);
+    // UserEingabe ist meistens String. gleich mal checken.
+    console.log(typeof guess);
+
+    // a bisserl DOm mainpulieren
+    //document.querySelector('.message').textContent = "SUPER 🎊 GEIL !!";
 });
