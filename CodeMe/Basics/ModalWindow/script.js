685d41e106662c1b9b886382699c6f326586cd2a
Mchoeti <lastcrusade@gmx.at>
2021-12-10 17:25:14 +0100

    added Functions for Modal

--- CodeMe/Basics/ModalWindow/script.js
+++ CodeMe/Basics/ModalWindow/script.js
@@ -10,5 +10,48 @@ console.log(btnsOpenModal);
 
 // Log Text Content to the Console
 for (let i = 0; i < btnsOpenModal.length; i++) {
-    console.log(btnsOpenModal[i].textContent);
+    // console.log(btnsOpenModal[i].textContent);
+    // Checken ob das mit dem Button auch klappt
+
+    const openModal = function () {
+        console.log("Button Clicked");
+        // the . is only for the selector. Da muss der String wirklich passen
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    };
+
+    btnsOpenModal[i].addEventListener('click', openModal);
+
+    // gleich mal als Function einbauen
+    const closeModal = function () {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    };
+
+    btnCloseModal.addEventListener('click', closeModal);
+    overlay.addEventListener('click', closeModal);
+
 }
+
+/* Zuerst habe ich die simple Variante verwendet
+
+// Simples Öffnen 
+    btnsOpenModal[i].addEventListener('click', function () {
+            console.log("Button Clicked");
+            // the . is only for the selector. Da muss der String wirklich passen
+            modal.classList.remove('hidden');
+            overlay.classList.remove('hidden');
+        })
+
+//das wäre Simples Schließen
+    btnCloseModal.addEventListener('click', function () {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    });
+
+    overlay.addEventListener('click', function () {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    });
+
+    */
