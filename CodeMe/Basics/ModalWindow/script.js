2e09556aad83ff7396e3a71b9b479796cb994ba4
Mchoeti <lastcrusade@gmx.at>
2021-12-14 21:33:16 +0100

    Modal Project ready

--- CodeMe/Basics/ModalWindow/script.js
+++ CodeMe/Basics/ModalWindow/script.js
@@ -14,7 +14,7 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
     // Checken ob das mit dem Button auch klappt
 
     const openModal = function () {
-        console.log("Button Clicked");
+        //console.log("Button Clicked");
         // the . is only for the selector. Da muss der String wirklich passen
         modal.classList.remove('hidden');
         overlay.classList.remove('hidden');
@@ -30,8 +30,27 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
     btnCloseModal.addEventListener('click', closeModal);
     overlay.addEventListener('click', closeModal);
-
 }
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+
+// Globales Event, ein Keypress Event
+// nehmen wir mal e für Event
+document.addEventListener("keydown", function (e) {
+    //console.log("A Key was pressed");
+    // mal schauen was wir drücken 
+    console.log(e);
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        console.log("Well done you pressed the Escape button");
+        closeModal();
+        // if modal class is NOT hidden mach es zu
+        console.log("Modal now is hidden");
+    }
+});
 
 /* Zuerst habe ich die simple Variante verwendet
 
@@ -53,5 +72,4 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
         modal.classList.add('hidden');
         overlay.classList.add('hidden');
     });
-
-    */
+*/
