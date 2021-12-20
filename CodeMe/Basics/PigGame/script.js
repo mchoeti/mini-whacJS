11595b256148621ecbdded1e7aed8fe9cb7d138a
Mchoeti <lastcrusade@gmx.at>
2021-12-20 16:34:44 +0100

    divining some start conditions

--- CodeMe/Basics/PigGame/script.js
+++ CodeMe/Basics/PigGame/script.js
@@ -4,25 +4,37 @@
 const resetScore = document.querySelector(".score");
 const resetValue = 0;
 
+// mal ein paar Vars definieren
+const score0EL = document.querySelector("#score--0");
+const score1EL = document.getElementById("score--1");
+const resetDice = document.querySelector(".dice");
+
+// Werte auf 0 setzen beim Start
+score0EL.textContent = resetValue;
+score1EL.textContent = resetValue;
+resetDice.classList.add("hidden");
 
 
+// Reset definieren
 document.querySelector(".btn--new").addEventListener("click", function () {
     console.log("You pressed the Reset");
-    document.querySelector("#score--0").textContent = resetValue;
-    document.querySelector("#score--1").textContent = resetValue;
+    score0EL.textContent = resetValue;
+    score1EL.textContent = resetValue;
+    // document.querySelector("#score--0").textContent = resetValue;
+    // document.querySelector("#score--1").textContent = resetValue;
     //document.querySelector("#current--0").textContent = resetValue;
     //document.querySelector("#current--1").textContent = resetValue;
     document.getElementById("current--0").textContent = " 67 ";
     document.getElementById("current--1").textContent = "22";
-
     //document.querySelectorAll("#score--0", #score--1).textContent = resetValue;
-
 });
 
+// Roll the dice definieren.
 document.querySelector(".btn--roll").addEventListener("click", function () {
     console.log("Roll the dice");
 });
 
+// HOld you Roll and pass to the next player
 document.querySelector(".btn--hold").addEventListener("click", function () {
     console.log("Hold me now !!!");
 });
