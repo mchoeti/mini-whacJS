aaf9c8d405cba33e88743eb26a16c22f2a28a6ad
Mchoeti <lastcrusade@gmx.at>
2021-12-21 12:36:41 +0100

    implemented Dice und Co

--- CodeMe/Basics/PigGame/script.js
+++ CodeMe/Basics/PigGame/script.js
@@ -4,15 +4,47 @@
 const resetScore = document.querySelector(".score");
 const resetValue = 0;
 
+// Score definieren, muss ein let sein, weil wir updaten :-)
+let currentScore = 0;
+
 // mal ein paar Vars definieren
 const score0EL = document.querySelector("#score--0");
 const score1EL = document.getElementById("score--1");
-const resetDice = document.querySelector(".dice");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 // Werte auf 0 setzen beim Start
 score0EL.textContent = resetValue;
 score1EL.textContent = resetValue;
-resetDice.classList.add("hidden");
+diceEl.classList.add("hidden");
+
+
+// Roll the Dice Function
+btnRoll.addEventListener("click", function () {
+    // Generate a random rolled
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log("Der Wert ist: ", dice);
+    // display the Dice und dann wechseln wir auch gleich das Bild
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
+
+
+    // check if dice == 1
+    if (dice !== 1) {
+        console.log("Go on...");
+        currentScore += dice;
+        current0El.textContent = currentScore;
+
+    } else {
+        console.log("Der Wert is 1, Spielerwechsel");
+        current0El.textContent = "0";
+    }
+});
+
 
 
 // Reset definieren
@@ -26,6 +58,7 @@ document.querySelector(".btn--new").addEventListener("click", function () {
     //document.querySelector("#current--1").textContent = resetValue;
     document.getElementById("current--0").textContent = " 67 ";
     document.getElementById("current--1").textContent = "22";
+    diceEl.classList.add("hidden");
     //document.querySelectorAll("#score--0", #score--1).textContent = resetValue;
 });
 
