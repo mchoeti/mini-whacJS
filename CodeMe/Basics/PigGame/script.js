7ebe28edc412623db8c69da664eb90c81b99ff66
Mchoeti <lastcrusade@gmx.at>
2022-01-05 17:06:41 +0100

    init function eingebaut

--- CodeMe/Basics/PigGame/script.js
+++ CodeMe/Basics/PigGame/script.js
@@ -3,12 +3,12 @@
 
 const resetScore = document.querySelector(".score");
 const resetValue = 0;
-const scores = [0, 0];
+//const scores = [0, 0];
 
 // Score definieren, muss ein let sein, weil wir updaten :-)
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+//let currentScore = 0;
+//let activePlayer = 0;
+//let playing = true;
 
 // mal ein paar Vars definieren
 const player0E = document.querySelector(".player--0");
@@ -24,7 +24,28 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-//let scores, currentScore, activePlayer, playing;
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+    console.log("New Game launches");
+    // Score definieren, muss ein let sein, weil wir updaten :-)
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+    // Werte auf 0 setzen beim Start
+    score0EL.textContent = 0;
+    score1EL.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    diceEl.classList.add("hidden");
+    player0E.classList.remove("player--winner");
+    player1E.classList.remove("player--winner");
+    player0E.classList.add("player--active");
+    player1E.classList.remove("player--active");
+};
+
+init();
 
 // Code niemals 2 mal verwenden daher DRY eine Funktion brauchen
 // und weil wir keine Parameter brauchen, Code is ja gleich kann man das so super einbauen
@@ -100,45 +121,4 @@ btnHold.addEventListener("click", function () {
 
 
 // Reset definieren
-btnNew.addEventListener('click', function () {
-    console.log("New Game launches");
-    // Score definieren, muss ein let sein, weil wir updaten :-)
-    currentScore = 0;
-    activePlayer = 0;
-    playing = true;
-    // Werte auf 0 setzen beim Start
-    score0EL.textContent = 0;
-    score1EL.textContent = 0;
-    current0El.textContent = 0;
-    current1El.textContent = 0;
-    diceEl.classList.add("hidden");
-
-});
-
-
-
-
-
-// document.querySelector(".btn--new").addEventListener("click", function () {
-//     console.log("You pressed the Reset");
-//     // var wieder auf true setzen
-//     playing = true;
-//     // winner Klasse removen 
-//     player0E.classList.remove(".player-winner");
-//     player1E.classList.remove(".player-winner");
-//     player0E.classList.add(".player-active");
-//     player1E.classList.remove(".player-active");
-
-
-
-//     score0EL.textContent = resetValue;
-//     score1EL.textContent = resetValue;
-//     // document.querySelector("#score--0").textContent = resetValue;
-//     // document.querySelector("#score--1").textContent = resetValue;
-//     //document.querySelector("#current--0").textContent = resetValue;
-//     //document.querySelector("#current--1").textContent = resetValue;
-//     document.getElementById("current--0").textContent = " 0.0 ";
-//     document.getElementById("current--1").textContent = "0.0";
-//     diceEl.classList.add("hidden");
-//     //document.querySelectorAll("#score--0", #score--1).textContent = resetValue;
-// });
+btnNew.addEventListener('click', init);
